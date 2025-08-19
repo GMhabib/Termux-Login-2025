6ff2bd835a8d5af57fd739e44a11f2a5526de43b
habibGM <72284417+GMhabib@users.noreply.github.com>
2025-08-19 22:21:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,11 +59,40 @@ echo "pkill -f zsh" >> menu
 echo "pkill -f bash" >> menu
 chmod +x menu
 pkg install neofetch -y
+clear
+# Command history tweaks:
+# - Append history instead of overwriting
+#   when shell exits.
+# - When using history substitution, do not
+#   exec command immediately.
+# - Do not save to history commands starting
+#   with space.
+# - Do not save duplicated commands.
+echo "shopt -s histappend" > $PREFIX/etc/bash.bashrc
+echo "shopt -s histverify" >> $PREFIX/etc/bash.bashrc
+echo "export HISTCONTROL=ignoreboth" >> $PREFIX/etc/bash.bashrc
+
+# Default command line prompt.
+echo "PROMPT_DIRTRIM=2" >> $PREFIX/etc/bash.bashrc
+# Test if PS1 is set to the upstream default value, and if so overwrite it with our default.
+# This allows users to override $PS1 by passing it to the invocation of bash as an environment variable
+echo '[[ "$PS1" == "\\s-\\v\\$ " ]] && PS1="\\[\\e[0;32m\\]\\w\\[\\e[0m\\] \\[\\e[0;97m\\]\\$\\[\\e[0m\\] "' >> $PREFIX/etc/bash.bashrc
+# Handles nonexistent commands.
+# If user has entered command which invokes non-available
+# utility, command-not-found will give a package suggestions.
+echo "if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then " >> $PREFIX/etc/bash.bashrc
+echo '	command_not_found_handle() {
+		/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1" ' >> $PREFIX/etc/bash.bashrc
+echo "	} " >> $PREFIX/etc/bash.bashrc
+echo "fi" >> $PREFIX/etc/bash.bashrc
+
+echo "[ -r /data/data/com.termux/files/usr/share/bash-completion/bash_completion ] && . /data/data/com.termux/files/usr/share/bash-completion/bash_completion " >> $PREFIX/etc/bash.bashrc
 echo "neofetch -f > $PREFIX/etc/motd " >> $PREFIX/etc/bash.bashrc
 echo "cat $PREFIX/etc/menu > $HOME/menu" >> $PREFIX/etc/bash.bashrc
 echo "chmod +x $HOME/menu " >> $PREFIX/etc/bash.bashrc
 echo "bash $HOME/menu " >> $PREFIX/etc/bash.bashrc
 clear
+
 echo "install Completed"
 sleep 2
 pkill -f termux
